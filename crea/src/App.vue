436936c3f02d8f7dc2d6061e8de906a858963bf0
Zan Jovanovic <zanjovanovic117@gmail.com>
2020-11-15 12:23:02 +0100

    initial commit

---
+++ crea/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="menu">
+      <h2>Menu</h2>
+      <div class="hamburger">
+        <span class="bar1"></span>
+        <span class="bar2"></span>
+        <span class="bar3"></span>
+      </div>
+    </div>
+    <Header msg="Header mesage"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+* {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
